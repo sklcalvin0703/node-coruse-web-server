c9efffd0882cdbf754aaf4be2d87e6518c247f6b
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2018-12-27 23:42:55 +0800

    Add Project Page

--- server.js
+++ server.js
@@ -68,6 +68,12 @@ app.get('/bad', (req,res)=>{
         errorMessage: 'error'
     })
 });
+
+app.get('/projects', (req, res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projects'
+    });
+});
 //bind the application to the port local nachine
 app.listen(port, ()=>{
     console.log(`Server is up on port ${port}`);
